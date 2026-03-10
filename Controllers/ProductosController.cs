2c1fc68cceddde76fddde45ac4ce723c7e6cedbf
Alex Sullon <alexsullonporras@hotmail.com>
2026-03-09 20:31:18 -0500

    feat: Se implementó la actualización de los productos

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -28,12 +28,28 @@ namespace VeterinariaWeb.Controllers
             return View(new Producto());
         }
 
-
         [HttpPost]
         public IActionResult Create(Producto producto)
         {
             var exito = CrearProducto(producto);
             return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var productoBuscado = obtenerProductoPorId(id);
+            var categorias = obtenerCategorias();
+            ViewBag.Categorias = new SelectList(categorias, "ID", "Nombre");
+            return View(productoBuscado);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Producto producto)
+        {
+            var exito = ActualizarProducto(producto);
+            if(exito)
+                return RedirectToAction("Detail", new { id = producto.ID});
+            return View(producto);
         }
 
         #region . Private methods .
@@ -124,6 +140,26 @@ namespace VeterinariaWeb.Controllers
             return exito;
         }
 
+        private bool ActualizarProducto(Producto producto)
+        {
+            var exito = false;
+            using (var conexion = new SqlConnection(cadenaConexion))
+            {
+                using (var comando = new SqlCommand("UPDATE Productos SET Nombre = @nombre, Descripcion = @descripcion, " +
+                    "Precio = @precio, CategoriaID = @categoria WHERE ID = @id", conexion))
+                {
+                    comando.Parameters.AddWithValue("@nombre", producto.Nombre);
+                    comando.Parameters.AddWithValue("@descripcion", producto.Descripcion);
+                    comando.Parameters.AddWithValue("@precio", producto.Precio);
+                    comando.Parameters.AddWithValue("@categoria", producto.CategoriaID);
+                    comando.Parameters.AddWithValue("@ID", producto.ID);
+                    conexion.Open();
+                    exito = comando.ExecuteNonQuery() > 0;
+                }
+            }
+            return exito;
+        }
+
         private Producto convertirReaderEnProducto(SqlDataReader lector)
         {
             return new Producto()
@@ -131,7 +167,7 @@ namespace VeterinariaWeb.Controllers
                 ID = lector.GetInt32(0),
                 Nombre = lector.GetString(1),
                 Descripcion = lector.GetString(2),
-                Imagen = lector.GetString(3),
+                Imagen = lector["PathImagen"] == DBNull.Value ? "":  lector.GetString(3),
                 Precio = lector.GetDecimal(4),
                 CategoriaID = lector.GetInt32(5),
                 Categoria = new Categoria()
